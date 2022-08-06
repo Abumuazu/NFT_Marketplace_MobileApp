b7b017951987f1cd302681d1419cbeea6da4da75
MUAZU <kakupepe09@gmail.com>
2022-08-06 17:27:07 +0100

    added contants components and setup Focused statusBar component

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,41 @@
 import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { ScrollView } from "react-native";
+import { COLORS, NFTData } from "../constants";
+import { NFTCard } from "../components";
+ 
 const HomeScreen = () => {
+  const _renderItem= ({ item }) => <NFTCard data={item} />
   return (
-    <View>
-    <Text>Home screem</Text>
-  </View>
+    <SafeAreaView style={{ flex: 1 }}>
+    <FocusedStatusBar backgroundColor={COLORS.primary} />
+    <View style={{ flex: 1 }}>
+      <View style={{ zIndex: 0 }}>
+        <FlatList
+          data={nftData}
+          renderItem={_renderItem}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+        />
+      </View>
+
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <View
+          style={{ height: 300, backgroundColor: COLORS.primary }} />
+        <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+      </View>
+    </View>
+  </SafeAreaView>
   );
 };
 
